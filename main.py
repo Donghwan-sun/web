aacba88846188c6208591622d752b6f5d08317ea
sundonghwan <sunehd564@gmail.com>
2022-04-13 23:51:07 +0900

    delete test script

--- main.py
+++ main.py
@@ -35,6 +35,27 @@ def hello():
     h1_html_tag = html_creator("h1")
     #h1_html_tag("Hello World")
     return h1_html_tag("Hello World")
+from bs4 import BeautifulSoup
+import requests
+
+def html_creator(tag, new_tag):
+    def text_wrapper(text):
+        new_text = text.replace(tag, new_tag)
+        print(f"{new_text}")
+    return text_wrapper
+
+#star = html_creator("*")
+#star(["안녕", "하세요"])
+
+res = requests.get("https://davelee-fun.github.io/blog/crawl_html_css.html")
+soup = BeautifulSoup(res.text, "html.parser")
+link_titles = soup.select("ul#hobby_course_list > li")
+for link_title in link_titles:
+    translate_text = html_creator("-", "*")
+    text = link_title.get_text()
+    translate_text(link_title.get_text())
+
+
 
 if __name__ == "__main__":
     app.run(host=host_addr, port=port_num, debug=True)
